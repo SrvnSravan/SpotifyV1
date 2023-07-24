591001408efe22254e460f543274a822b5d54eaa
Srvn Sravan <srvnsravan46@gmail.com>
2023-07-24 12:14:06 +0530

    SearchBar added

--- App.js
+++ App.js
@@ -5,13 +5,13 @@ import HomeScreen from './app/screens/main-screens/HomeScreen';
 import Navigation from './app/navigators/Navigation';
 import {NavigationContainer} from '@react-navigation/native';
 import SplashScreen from './app/screens/SplashScreen';
-import { SearchBar } from 'react-native-screens';
+import SearchBar from './app/components/SearchBar';
 import SearchScreen from './app/screens/main-screens/SearchScreen';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <SearchScreen />
+      <SearchBar />
     </NavigationContainer>
   );
 };
---
+++ app/components/SearchBar.js
@@ -0,0 +1,56 @@
+import { StyleSheet, Text, View, TextInput, Image} from 'react-native'
+import React from 'react'
+import LinearGradient from 'react-native-linear-gradient'
+
+const SearchBar = () => {
+    // const [searchText, setSearchText] = useState('');
+  
+    // const handleSearch = (text) => {
+    //   // You can perform any search logic here
+    //   // For example, filter results based on the search text
+    //   setSearchText(text);
+    // };
+  
+    return (
+    <LinearGradient colors={['#040306', '#131624']} style={{flex: 1}}>
+      <View style={styles.container}>
+        <Image source={require('../assets/images/search.png')}
+        style={styles.searchIcon}/>
+        <TextInput
+          style={styles.input}
+          placeholder="What do you want to listen to?"
+          placeholderTextColor="#000000"
+          textAlign='center'
+        />
+      </View>
+      </LinearGradient>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      backgroundColor: '#F5F5F5',
+      borderRadius: 8,
+      marginHorizontal: 16,
+      marginVertical: 8,
+      padding: 8,
+      height: 50
+    },
+    searchIcon: {
+      height: 30,
+      width: 30
+    },
+    input: {
+      flex: 1,
+      fontSize: 16,
+      fontWeight: "bold",
+      color: '#000000',
+      height: 40
+      
+    },
+  });
+  
+  export default SearchBar;
+  
