69616bc49a0600341aac31e76726308521ffb472
Srvn Sravan <srvnsravan46@gmail.com>
2023-07-25 12:37:32 +0530

    Added some modifications in PremiumScreen

--- app/components/NormalButton.js
+++ app/components/NormalButton.js
@@ -1,48 +1,45 @@
 import {
-    StyleSheet,
-    Text,
-    View,
-    Pressable,
-    TouchableOpacity,
-  } from 'react-native';
-  import React from 'react';
-  
-  const NormalButton = ({title, onPress, containerStyle}) => {
-   
-    return (
-      <View>
-        <TouchableOpacity style={styles.pressable}
-        onPress={onPress}>
-          <Text style={styles.buttonText}>{title}</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  };
-  
-  export default NormalButton;
-  
-  const styles = StyleSheet.create({
-    pressable: {
-      backgroundColor: 'white',
-      padding: 10,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      width: 300,
-      height: 50,
-      borderRadius: 25,
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginVertical: 5,
-    },
-    buttonText: {
-      fontWeight: 'bold',
-      color: 'black',
-      fontSize: 15
-    },
-    normalButtonText: {
-      fontWeight: 'bold',
-      color: 'white',
-      fontSize: 15
-    },
-  });
-  
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
+
+const NormalButton = ({title, onPress, containerStyle}) => {
+  return (
+    <View>
+      <TouchableOpacity style={styles.pressable} onPress={onPress}>
+        <Text style={styles.buttonText}>{title}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default NormalButton;
+
+const styles = StyleSheet.create({
+  pressable: {
+    backgroundColor: 'white',
+    padding: 10,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: 300,
+    height: 50,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 5,
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    color: 'black',
+    fontSize: 15,
+  },
+  normalButtonText: {
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 15,
+  },
+});
--- app/components/PlanCard.js
+++ app/components/PlanCard.js
@@ -1,41 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import NormalButton from './NormalButton'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import NormalButton from './NormalButton';
 
-const PlanCard = ({ backgroundColor, title, title1, description, description1, description2}) => {
+const PlanCard = ({
+  backgroundColor,
+  title,
+  title1,
+  description,
+  description1,
+  description2,
+}) => {
   return (
     <View style={[styles.cardContainer, {backgroundColor}]}>
-        <View style={{flexDirection: "row", justifyContent: "space-between"}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
         <Text style={styles.cardsHeading}>{title}</Text>
         <Text style={styles.cardsHeading}>{title1}</Text>
-        </View>
-        <View style={{justifyContent: "center", alignItems: "center"}}>
+      </View>
+      <View style={{justifyContent: 'center', alignItems: 'center'}}>
         <Text style={styles.cardsSubHeading}>{description}</Text>
-        <NormalButton title={'VIEW PLANS'}/>
+        <NormalButton title={'VIEW PLANS'} />
         <Text style={styles.cardsSubHeading}>{description1}</Text>
         <Text style={styles.cardsSubHeading}>{description2}</Text>
-        </View>
       </View>
-  )
-}
+    </View>
+  );
+};
 
-export default PlanCard
+export default PlanCard;
 
 const styles = StyleSheet.create({
-    cardContainer: {
-        // width: 370,
-        // height: 300,
-        borderRadius: 10,
-        margin: 10,
-      },
-      cardsHeading: {
-        color: "white",
-        fontSize: 30,
-        margin: 20
-      },
-      cardsSubHeading: {
-        color: "white",
-        padding: 15,
-        marginLeft: 10
-      }
-})
+  cardContainer: {
+    // width: 370,
+    // height: 300,
+    borderRadius: 10,
+    margin: 10,
+  },
+  cardsHeading: {
+    color: 'white',
+    fontSize: 30,
+    margin: 20,
+  },
+  cardsSubHeading: {
+    color: 'white',
+    padding: 15,
+    marginLeft: 10,
+  },
+});
